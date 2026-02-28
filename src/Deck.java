c422e5f4cfd20b93e8cf6f81e268823dc76a42ba
Lukas Buysschaert <Lukas.Buysschaert@student.kuleuven.be>
2026-02-28 20:50:12 +0100

    Finish Deck Class implementation

--- src/Deck.java
+++ src/Deck.java
@@ -1,14 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Deck {
     private int PLAYER_COUNT = 4;
     private int DECK_COUNT = 52;
+    // when cards are dealt each player first gets 4 then 4 more and finally 5
+    private int[] dealSteps = {4, 4, 5};
 
     private ArrayList<Card> currentCards;
     private ArrayList<String> players;
     private Map<String, ArrayList<Card>> playerCards;
+
 
     private ArrayList<Card> getDeckCards() {
         ArrayList<Card> cards = new ArrayList<Card>();
@@ -19,6 +23,15 @@ public class Deck {
         }
         return cards;
     }
+    private void dealPlayerCards(String player, int cardsAmount) {
+        for (int i = 0; i < cardsAmount; i++) {
+            ArrayList<Card> cardList = this.playerCards.get(player);
+            cardList.add(this.currentCards.getLast());
+            this.currentCards.remove(this.currentCards.toArray().length - 1);
+        }
+    }
+
+    // ======================= Public =======================
 
     // playerNames needs to be in order from left to right
     public Deck(ArrayList<String> playerNames) {
@@ -30,10 +43,24 @@ public class Deck {
     }
 
     // returns temporary trump suit (last card dealt)
-    public Card.Suit DealCards() {}
+    public Card.Suit DealCards() {
+        for (int cardsAmount : this.dealSteps) {
+            for (String player : this.players) {
+                dealPlayerCards(player, cardsAmount);
+            }
+        }
+        Card lastCard = this.playerCards.get(this.players.getLast()).getLast();
+        return lastCard.getSuit();
+    }
 
-    public ArrayList<Card> getCards(String playerName) {}
+    public ArrayList<Card> getCards(String playerName) {
+        ArrayList<Card> playerCards = this.playerCards.get(playerName);
+        // makes a shallow copy (should be okay if cards are immutable)
+        return (ArrayList<Card>) playerCards.clone();
+    }
 
     // add cards to the deck
-    public void returnCards(ArrayList<Card> cards) {}
+    public void returnCards(ArrayList<Card> cards) {
+        this.currentCards.addAll(cards);
+    }
 }
